82b584a2ff471224f73baab96cb249df42d7dca5
anil verma <anilcs240@gmail.com>
2021-04-08 09:24:25 +0530

    weave with queue

--- queue.js
+++ queue.js
@@ -31,3 +31,71 @@ q.add(2)
 q.add(3)
 
 console.log(q.remove())
+
+
+
+
+//another weave prob
+/*
+weave receives two queues as arguments and combine the content of each into new(third queue).
+third queue should contain the alternating of two queues
+
+Backbone.$ = $;
+
+    the function should handle queues of diffrent length without inserting 'undefined'  into new one.
+
+
+*/
+
+
+class Queue {
+
+    constructor() {
+        this.data = []
+
+    }
+
+    add(data) {
+        this.data.unshift(data)
+
+    }
+
+    remove() {
+        return this.data.pop()
+    }
+
+    peek() {
+        return this.data[this.data.length - 1]
+    }
+
+}
+
+const q1 = new Queue()
+const q2 = new Queue()
+
+q1.add(2)
+q1.add(3)
+q2.add(1)
+
+
+
+function weave(q1, q2) {
+    const q = new Queue()
+    while (q1.peek() || q2.peek()) {
+        if (q1.peek()) {
+            q.add(q1.remove())
+        }
+        if (q2.peek()) {
+            q.add(q2.remove())
+        }
+    }
+    console.log(q)
+    return q;
+}
+
+weave(q1, q2)
+
+
+
+
+
